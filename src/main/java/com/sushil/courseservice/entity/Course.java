62a87146d9b8b33027dee3a7c527c4aa13ca17c0
sushil <sushil.verma@walkingtree.tech>
2023-05-24 06:37:12 +0530

    added course entity

---
+++ src/main/java/com/sushil/courseservice/entity/Course.java
@@ -0,0 +1,65 @@
+package com.sushil.courseservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String courseName;
+
+    private Double duration;
+
+    public Course() {
+    }
+
+    public Course(Long id, String courseName, Double duration) {
+        this.id = id;
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
