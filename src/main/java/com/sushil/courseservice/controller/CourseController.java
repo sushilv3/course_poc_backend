8f6e5e7c46a6bc387ef1aefc62ad606ade1be93f
sushil <sushil.verma@walkingtree.tech>
2023-05-26 10:47:33 +0530

    added logger

--- src/main/java/com/sushil/courseservice/controller/CourseController.java
+++ src/main/java/com/sushil/courseservice/controller/CourseController.java
@@ -2,6 +2,8 @@ package com.sushil.courseservice.controller;
 
 import com.sushil.courseservice.entity.Course;
 import com.sushil.courseservice.service.CourseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @RequestMapping("/api/course")
 public class CourseController {
     private CourseService courseService;
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
 
     @Autowired
     public CourseController(CourseService courseService) {
--- src/main/java/com/sushil/courseservice/service/CourseService.java
+++ src/main/java/com/sushil/courseservice/service/CourseService.java
@@ -2,6 +2,8 @@ package com.sushil.courseservice.service;
 
 import com.sushil.courseservice.entity.Course;
 import com.sushil.courseservice.repository.CourseRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import java.util.List;
 
 @Service
 public class CourseService implements CourseServiceImpl {
+    private static final Logger logger = LoggerFactory.getLogger(CourseService.class);
 
     private CourseRepository courseRepository;
 
